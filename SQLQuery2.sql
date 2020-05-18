8a2321f13c8ac51ffa7d2e688ba738bf3e08eab9
MotocMihai <mihaimotoc46@yahoo.com>
2020-05-18 17:03:10 +0300

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ContactID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NCHAR(10) NULL, 
+    [Company] NCHAR(10) NULL,
+	[Email] NCHAR(10) NULL, 
+    [Client] NCHAR(10) NULL, 
+    [Last Call] NCHAR(10) NULL,
+)
---
+++ dbo.Table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ContactId] INT NOT NULL IDENTITY , 
+  
+    [Name] VARCHAR(50) NULL, 
+    [Company] NVARCHAR(50) NULL, 
+    [Telephone] VARCHAR(50) NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [Client] BIT NULL, 
+    [Last call] DATETIME NULL, 
+    PRIMARY KEY ([ContactId])
+)
